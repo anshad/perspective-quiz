743e13fd47cab232b11dc3743071ea07239cd722
anshad <anshad.musafir@gmail.com>
2020-12-06 21:55:06 +0530

    Add questions RESTful API

---
+++ api/app/Http/Controllers/QuestionController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Question;
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Question::all();
+    }
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// TODO: add other CRUD options later
+Route::apiResource('question', QuestionController::class)->only(['index']);
